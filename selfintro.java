70a3947de92823ba48686af0fec8c6f753886bb2
Aditya Kumar Pandit <adityakumarpandit643@gmail.com>
2026-07-10 08:14:42 +0530

    Add files via upload

---
+++ selfintro.java
@@ -0,0 +1,19 @@
+//1.1 write a java program to get your selfintro.
+
+
+class selfintro
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Good morning everyone.");
+			System.out.println("My name is  Aditya Kumar Pandit");
+			System.out.println("I am from Bihar");
+			System.out.println("Currently, I am Pursuing Bachelor of Computer Applications (BCA) at Marwadi University");
+			System.out.println("My hobbies are playing cricket and studying");
+			System.out.println("I have learned HTML, CSS, JavaScript, SQL, and Java");
+			System.out.println("I enjoy building websites and improving my programming skills");
+			System.out.println("I enjoy coding");
+			System.out.println("I want to become a software developer");
+			System.out.println("Thank u");
+		}
+}
