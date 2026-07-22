2725ea89ae0b6bfc8fd5369d75ff0d4247ff2ffa
Aditya Kumar Pandit <adityakumarpandit643@gmail.com>
2026-07-22 22:21:46 +0530

    prg4.java

--- Unit1/prg4.java
+++ Unit1/prg4.java
@@ -1,21 +1,18 @@
-//WRITE A JAVA PROGRAM TO PERFORM DIFFERENT ARITHMETIC OPERATION WITHOUT COMMAND LINE ARGS
+// 4.WRITE A JAVA PROGRAM TO PERFORM DIFFERENT ARITHMETIC OPERATION USING (COMMAND LINE ARGS).
 
 import java.util.Scanner;
 
 public class prg4{
-    public static void main(String args []) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your firstNumber: ");
-        int a = input.nextInt();
-
-	System.out.print("Enter your secondNumber: ");
-        int b = input.nextInt();
-
-	 System.out.println(a+b);
- 	 System.out.println(a-b);
-	 System.out.println(a*b);
-         System.out.println(a/b);
 	
-     
-	}
+    public static void main(String args[]) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b));
+    }
 }
