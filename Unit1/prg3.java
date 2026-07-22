34cc1b11c484aa448a1a6db6e942664a79df1460
Aditya Kumar Pandit <adityakumarpandit643@gmail.com>
2026-07-22 21:51:30 +0530

    prg3.java

--- Unit1/prg3.java
+++ Unit1/prg3.java
@@ -1,28 +1,31 @@
-//WRITE A JAVA PROGRAM TO GET PERSONAL INFORMATION FROM USER AND DISPLAY ON SCREEN.
+//  3.WRITE A JAVA PROGRAM TO GET PERSONAL INFORMATION FROM USER AND DISPLAY ON SCREEN.
 
 import java.util.Scanner;
 
-	class prg3{
-		
+	public class test{		
 	public static void main(String args []) {
         	Scanner input = new Scanner(System.in);
+			
         	System.out.print("Enter your name: ");
         	String name = input.nextLine();
+			
         	System.out.print("Enter your Cource: ");
         	String cource = input.nextLine();
+			
         	System.out.print("Enter batch: ");
         	String div = input.nextLine();
          
         	System.out.print("Enter class: ");
         	String cl = input.nextLine();
+			
         	System.out.print("Enter roll: ");
         	int roll = input.nextInt();
         
         System.out.println(name);
-   	System.out.println(cource);
-	System.out.println(div);
-	System.out.println(cl);
-	System.out.println(roll);
+   		System.out.println(cource);
+		System.out.println(div);
+		System.out.println(cl);
+		System.out.println(roll);
 	 
 	}
 }
