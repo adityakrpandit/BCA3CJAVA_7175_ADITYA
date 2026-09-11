95f9ad32c936cdee318fe6f396411ba6bf78accb
Aditya Kumar Pandit <adityakumarpandit643@gmail.com>
2026-09-12 01:10:57 +0530

    Add files via upload

---
+++ Unit 2/Student/Student.java
@@ -0,0 +1,11 @@
+package school.students;
+
+public class Student{
+    public void displayInfo(){
+
+        String s1 = "Vivan";
+        String s2 = "Aadi";
+        
+        System.out.println("Class Toppers:" + s1 + "and" + s2);
+    }
+}
---
+++ Unit 2/Student/mainapp.java
@@ -0,0 +1,10 @@
+import school.students.Student;
+
+public class mainapp {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.displayInfo();
+        
+    }
+}
